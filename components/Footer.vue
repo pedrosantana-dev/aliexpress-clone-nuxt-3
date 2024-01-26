51ff8aab9dddef213d3deeeecff1c7656260e38d
Pedro Santana <pedromanu27@gmail.com>
2024-01-26 13:21:37 -0300

    feat: MainLayout finalizado e index page

---
+++ components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+	<div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+		<div class="max-w-[1200px] mx-auto px-4">
+			<div class="md:flex gap-4 justify-between w-full">
+				<div class="md:w-[50%] pt-4">
+					<div class="text-lg font-semibold text-[#282828]">Ajuda</div>
+					<div class="text-[#999999] text-sm">
+						Central de Ajuda, Disputes & Reports, Buyer Protection, Denunciar
+						infração de propriedade intelectua, Regulated Information, Integrity
+						Compliance
+					</div>
+				</div>
+				<div class="md:w-[50%] pt-4">
+					<div class="text-lg font-semibold text-[#282828]">
+						AliExpress Multi-Language Sites
+					</div>
+					<div class="text-[#999999] text-sm">
+						Pусский, Português, Español, Français, Deutsch, Italiano,
+						Nederlands, Türk, 日本語, 한국어, ภาษาไทย, tiếng Việt, اللغة العربي,
+						עברית, Polish
+					</div>
+				</div>
+			</div>
+
+			<div class="md:flex gap-4 justify-between w-full">
+				<div class="md:w-[50%] pt-4">
+					<div class="text-lg font-semibold text-[#282828]">
+						Buscar por Categoria
+					</div>
+					<div class="text-[#999999] text-sm">
+						Os Mais Vendidos, Produtos, Promoções, Preço, Barato, Comentários,
+						China Brands, Blog, Seller Portal, BLACK FRIDAY, Assistente do
+						AliExpress
+					</div>
+				</div>
+				<div class="md:w-[50%] pt-4">
+					<div class="text-lg font-semibold text-[#282828]">Alibaba Group</div>
+					<div class="text-[#999999] text-sm">
+						Alibaba Group Website, AliExpress, Alimama, Alipay, Fliggy, Alibaba
+						Cloud, Alibaba International, AliTelecom, DingTalk, Juhuasuan,
+						Taobao Marketplace, Tmall, Taobao Global, AliOS, 1688
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="FotterSub" class="w-full bg-[#333333] text-sm text-gray-400">
+		<div class="max-w-[1200px] mx-auto p-4">
+			Proteção de Propriedade Intelectual - Política de Privacidade - Mapa do
+			Site - Condições de Uso - Transaction Services Agreement for non-EU/UK
+			Consumers - Termos e Condições para os Consumidores da UE/EEE/Reino Unido
+			- Guia de Informações Jurídicas para o Usuário ©️ 2010-2023
+			AliExpress.com. Todos os direitos reservados.
+		</div>
+	</div>
+</template>
+
+<script setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,91 @@
 <template>
-    <MainLayout>
-        <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-            IndexPage
-        </div>
-    </MainLayout>
+	<MainLayout>
+		<div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+			<div
+				class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
+			>
+				<div v-if="products" v-for="product in products" :key="product">
+					<ProductComponent :product="product" />
+				</div>
+			</div>
+		</div>
+	</MainLayout>
 </template>
 
 <script setup>
-import MainLayout from '~/layouts/MainLayout.vue'
+	import MainLayout from '~/layouts/MainLayout.vue';
+	import ProductComponent from '~/components/ProductComponent.vue';
+
+	const products = [
+		{
+			id: 1,
+			title: 'Título 1',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 9999,
+		},
+		{
+			id: 2,
+			title: 'Título 2',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 9699,
+		},
+		{
+			id: 3,
+			title: 'Título 3',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 9999,
+		},
+		{
+			id: 4,
+			title: 'Título 4',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 29999,
+		},
+		{
+			id: 5,
+			title: 'Título 5',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 49999,
+		},
+		{
+			id: 6,
+			title: 'Título 6',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 94999,
+		},
+		{
+			id: 7,
+			title: 'Título 7',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 39999,
+		},
+		{
+			id: 8,
+			title: 'Título 8',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 93999,
+		},
+		{
+			id: 9,
+			title: 'Título 9',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 99599,
+		},
+		{
+			id: 10,
+			title: 'Título 10',
+			description: 'Essa é uma descrição',
+			url: 'https://picsum.photos/id/7/800/800',
+			price: 69999,
+		},
+	];
 </script>
