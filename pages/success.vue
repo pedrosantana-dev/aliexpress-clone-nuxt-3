de0f826d5a7884de328a6879dcc5b0616613ad25
Pedro Santana <pedromanu27@gmail.com>
2024-02-01 21:25:16 -0300

    feat: :sparkles: página de sucesso

---
+++ pages/success.vue
@@ -0,0 +1,29 @@
+<template>
+	<MainLayout>
+		<div id="SuccessPage" class="mt-4 max-w-[1200px] mx-auto px-2 min-h-[50vh]">
+			<div class="bg-white w-full p-6 min-h-[150px]">
+				<div class="flex items-center text-xl">
+					<Icon
+						name="clarity:success-standard-line"
+						color="#5FCB04"
+						size="35"
+					/>
+					<span class="pl-4">Pagamento realizado com sucesso!</span>
+				</div>
+				<p class="text-sm pl-[50px]">Obrigado! Recebemos seu pagamento.</p>
+			</div>
+		</div>
+	</MainLayout>
+</template>
+
+<script setup>
+	import MainLayout from '~/layouts/MainLayout.vue';
+	import { useUserStore } from '~/stores/user';
+	const userStore = useUserStore();
+
+	onMounted(() => {
+		setTimeout(() => (userStore.isLoading = false), 300);
+	});
+</script>
+
+<style></style>
